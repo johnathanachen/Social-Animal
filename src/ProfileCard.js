b4c026e920599ee68d183a6a67fe299ae37a9ca0
jc.swift <jc.swift@icloud.com>
2017-06-21 19:13:27 -0500

    added secuirty rules to firebase and total bytes transferred percentage in console

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -17,6 +17,11 @@ class ProfileCard extends Component {
     const file = event.target.files[0];
     const uploadTask = this.storageRef.child(file.name)
                                       .put(file, { contentType: file.type });
+
+    uploadTask.on('state_changed', (snapshot) => {
+      console.log(snapshot.bytesTransferred / snapshot.totalBytes * 100 + '%');
+    });
+
     uploadTask.then((snapshot) => {
       this.userRef.child('photoURL').set(snapshot.downloadURL);
     });
